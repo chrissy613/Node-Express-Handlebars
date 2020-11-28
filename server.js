72883823f1f9705e25c2554d9de86af2c793ca1f
Chris <cec213@scarletmail.rutgers.edu>
2020-11-28 18:09:10 -0500

    working deploy, needs css

--- server.js
+++ server.js
@@ -1,20 +1,17 @@
 const express = require("express");
 
-const PORT = process.env.PORT || 8000;
+const PORT = process.env.PORT || 3000;
 const app = express();
 
 app.use(express.static("public"));
-
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 const handlebars = require("express-handlebars");
-
 app.engine("handlebars", handlebars({ defaultLayout: "main" }));
 app.set("view engine", "handlebars");
 
-const routes = require("./controllers/burgersController.js");
-
+const routes = require("./controllers/burgers_controller.js");
 app.use(routes);
 
 app.listen(PORT, function() {
