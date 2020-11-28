77da982c5debc752e3834123c9260e3ccf8ebee9
Chris <cec213@scarletmail.rutgers.edu>
2020-11-28 18:25:59 -0500

    ready for deployment

--- public/assets/js/script.js
+++ public/assets/js/script.js
@@ -2,7 +2,6 @@ $(document).ready(function() {
   $("#devourBtn").on("submit", function(event) {
     event.preventDefault();
     var burger_id = $(this).children(".burger_id").val();
-    console.log(burger_id);
     $.ajax({
       method: "PUT",
       url: "/burgers/" + burger_id
