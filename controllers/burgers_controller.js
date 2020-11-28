030aa34be6bd6fd74fa2a500b1cceecd38dbf74d
Chris <cec213@scarletmail.rutgers.edu>
2020-11-28 17:02:19 -0500

    burgers_controller.js change

--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -13,7 +13,6 @@ router.get("/burgers", function(req, res) {
   });
 });
 
-// post route -> back to index
 router.post("/burgers/create", function(req, res) {
   burger.create(req.body.burger_name, function(result) {
     console.log(result);
@@ -21,7 +20,6 @@ router.post("/burgers/create", function(req, res) {
   });
 });
 
-// put route -> back to index
 router.put("/burgers/:id", function(req, res) {
   burger.update(req.params.id, function(result) {
     console.log(result);
